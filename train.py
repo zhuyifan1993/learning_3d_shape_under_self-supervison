40801c2f7fcecf09f37b4d71896c31afa9690896
Yifan Zhu <yifanzhu@github.com>
2020-11-21 11:00:26 +0100

    bug fix

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print('device:', device)
 
-    CONFIG_PATH = 'configs/shapenet.yaml'
+    CONFIG_PATH = 'configs/default.yaml'
     with open(CONFIG_PATH, 'r') as f:
         cfg = yaml.load(f, Loader=yaml.FullLoader)
 
